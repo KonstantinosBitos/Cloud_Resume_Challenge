dd1bb478ca9c6890afcca3ffe581901ea1d80fec
Kostis <bitoskostas1@gmail.com>
2025-12-12 21:38:05 +0200

    Removed duplicate providers from main.tf

--- infrastructure-v2/main.tf
+++ infrastructure-v2/main.tf
@@ -16,18 +16,6 @@ terraform {
   }
 }
 
-# ... rest of your file ...
-
-
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 5.0"
-    }
-  }
-}
-
 # The default provider 
 provider "aws" {
   region = "eu-north-1" 
